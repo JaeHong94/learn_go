7b3a7985a1440d8c039b1653f3471691a5001fc3
JaeHong94 <moonshin123@naver.com>
2021-10-01 00:00:47 +0900

    Golang: packages and imports

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/JaeHong94/learngo/sometings"
+)
 
 func main() {
+	sometings.SayHello()
 	fmt.Println("Hello world!")
 }
---
+++ sometings/sometings.go
@@ -0,0 +1,11 @@
+package sometings
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello")
+}
+
+func sayBye() {
+	fmt.Println("Bye")
+}
