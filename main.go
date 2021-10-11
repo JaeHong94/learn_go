98f6275ba3b2c713dd17a64b94a8cec04c46d60e
JaeHong94 <moonshin123@naver.com>
2021-10-11 19:09:21 +0900

    Golang: Switch

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	koreanAge := age + 2
-	if koreanAge > 19 {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 20:
 		return true
 	}
 	return false
